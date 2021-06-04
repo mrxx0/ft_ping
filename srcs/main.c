116c728628b15dcff11353d3d43956d2a9ffe43f
mrxx0 <chcoutur@student.42.fr>
2021-06-04 16:13:16 +0200

    Add root protection to execute ft_ping

--- srcs/main.c
+++ srcs/main.c
@@ -2,6 +2,11 @@
 
 int main(int argc, char **argv)
 {
+    if (getuid() != 0)
+    {
+        write(2, "ft_ping : You need to be root.\n", ft_strlen("ft_ping : You need to be root.\n"));
+        return (EXIT_FAILURE);
+    }
     if (argc > 1)
     {
         if (parsing_arguments(argc, argv) == EXIT_FAILURE)
