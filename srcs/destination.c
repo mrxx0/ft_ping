958db45d46d2d3b8d8dd1f0ed7a2c28d3064cba7
mrxx0 <chcoutur@student.42.fr>
2021-06-07 17:24:13 +0200

    Clean function get_destination

--- srcs/destination.c
+++ srcs/destination.c
@@ -1,47 +1,5 @@
 #include "../includes/ft_ping.h"
 
-_Bool    parse_address(char *str, t_payload *payload)
-{
-    size_t i = 0;
-    size_t dots = 0;
-    size_t members = 0;
-
-    while (str[i])
-    {
-        if (str[i] != '.' && ft_isdigit(str[i]) == EXIT_FAILURE)
-        {
-            printf("Failure for is digit\n");
-            return (EXIT_FAILURE);
-        }
-        else if (str[i] != '.')
-            members++;
-        if (str[i] == '.')
-        {
-            if (members > 3)
-            {
-                printf("Failure for members at %zu \n", members);
-                return (EXIT_FAILURE);
-            }
-            dots++;
-            members = 0;
-        }
-        i++;
-    }
-    if (i == ft_strlen(str))
-    {
-        if (dots == 3)
-        {
-            payload->address = ft_strdup(str);
-            payload->found_address = 1;
-            printf("Seems ok\n");
-            return(EXIT_SUCCESS);
-        }
-    }
-    
-    printf("Failure for size\n");
-    return (EXIT_FAILURE);     
-}
-
 _Bool    get_destination(int argc, char **argv, t_payload *payload)
 {
     int i = 1;
@@ -50,10 +8,10 @@ _Bool    get_destination(int argc, char **argv, t_payload *payload)
     {
         if (i != payload->id_opt && payload->found_address == 0)
         {
-            if (parse_address(argv[i], payload) == EXIT_FAILURE)
-                return (EXIT_FAILURE);
-            else
-                i++;
+            payload->address = ft_strdup(argv[i]);
+            if (payload->address == MALLOC_FAILED)
+                return (ft_perror("Malloc failed\n"));
+            return (EXIT_SUCCESS);
         }
         else
             i++;
