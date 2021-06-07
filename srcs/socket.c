f308bc3f1b4e4d7623fc1ec5eb76bdcdc445b5f4
mrxx0 <chcoutur@student.42.fr>
2021-06-07 16:29:41 +0200

    Fix TTL value

--- srcs/socket.c
+++ srcs/socket.c
@@ -5,9 +5,7 @@ _Bool init_socket(t_payload *payload)
     payload->socket_fd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (payload->socket_fd == -1)
         return (ft_perror("Can't create RAW socket\n"));
-    
-    payload->ttl = setsockopt(payload->socket_fd, IPPROTO_IP, IP_TTL, &payload->ttl, sizeof(payload->ttl));
-    if (payload->ttl < 0)
+    if (setsockopt(payload->socket_fd, IPPROTO_IP, IP_TTL, &payload->ttl, sizeof(payload->ttl)) < 0)
         return (ft_perror("Can't set TTL value\n"));
     return (EXIT_SUCCESS);
 }
