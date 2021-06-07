cca0d891b6bdac2bf14de961cf2773a6a3e175e8
mrxx0 <chcoutur@student.42.fr>
2021-06-07 16:24:27 +0200

    Fix payload declaration + add new function to print struct

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,19 @@
 #include "../includes/ft_ping.h"
 
+void print_payload(t_payload *payload)
+{
+    printf("socket_fd = [%d]\n", payload->socket_fd);
+    printf("ttl = [%d]\n", payload->ttl);
+    printf("opt = [%d]\n", payload->opt);
+    printf("id_opt = [%d]\n", payload->id_opt);
+    printf("address = [%s]\n", payload->address);
+    printf("found_address = [%d]\n", payload->found_address);
+}
+
 int main(int argc, char **argv)
 {
-    t_payload *payload = create_payload();
-
-    if (payload == MALLOC_FAILED)
-        return (EXIT_FAILURE);
+    t_payload *payload = NULL;
+    
     if (getuid() != 0)
     {
         write(2, "ft_ping : You need to be root.\n", ft_strlen("ft_ping : You need to be root.\n"));
@@ -13,6 +21,9 @@ int main(int argc, char **argv)
     }
     if (argc > 1)
     {
+        payload = create_payload();
+        if (payload == MALLOC_FAILED)
+            return (EXIT_FAILURE);
         if (parsing_arguments(argc, argv, payload) == EXIT_FAILURE)
         {
             free(payload->address);
@@ -26,6 +37,7 @@ int main(int argc, char **argv)
             free(payload);
             return (EXIT_FAILURE);
         }
+        print_payload(payload);
         free(payload->address);
         free(payload);
     }
