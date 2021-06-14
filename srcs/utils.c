e92f73ea15d62010bbf00b81866578362f37b654
mrxx0 <chcoutur@student.42.fr>
2021-06-14 14:13:18 +0200

    Add checksum function

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,5 +1,16 @@
 #include "../includes/ft_ping.h"
 
+uint16_t checksum(struct icmphdr *to_check, size_t size)
+{
+    uint16_t tmp = 0;
+
+    while (size-- != 0)
+    {
+        tmp -= (uint16_t)to_check++;
+    }
+    return (tmp);
+}
+
 void free_addrinfo(struct addrinfo *addr)
 {
     struct addrinfo *tmp;
