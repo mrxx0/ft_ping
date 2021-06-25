18c193baf6496dac9bb34a472cb96f5bda1a9653
charlesctr <chcoutur@student.42.fr>
2021-06-25 12:54:39 +0200

    Rewrite checksum part

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,28 +1,20 @@
 #include "../includes/ft_ping.h"
 
-// uint16_t checksum(struct icmphdr *to_check, size_t size)
-// {
-//     uint16_t tmp = 0;
-
-//     while (size-- != 0)
-//     {
-//         tmp -= (uint16_t)to_check++;
-//     }
-//     return (tmp);
-// }
-
 uint16_t checksum(void *to_check, size_t size)
 {
     unsigned short *buf = to_check;
-	unsigned int sum=0;
+	unsigned int tmp = 0;
 	unsigned short result;
 
-	for ( sum = 0; size > 1; size -= 2 )
-		sum += *buf++;
+	while (size > 1)
+	{
+		tmp += *buf++;
+		size = size -2;
+	}
 	if ( size == 1 )
-		sum += *(unsigned char*)buf;
-	sum = (sum >> 16) + (sum & 0xFFFF);
-	sum += (sum >> 16);
-	result = ~sum;
-	return result;
+		tmp += *(unsigned char*)buf;
+	tmp = (tmp >> 16) + (tmp & 0xFFFF);
+	tmp += (tmp >> 16);
+	result = ~tmp;
+	return (result);
 }
